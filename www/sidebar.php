ad3ae8f2bac7d85b2b260b3b4b0caf29cf693dfc
AapoNym <aapo.nyman@gmail.com>
2022-05-05 18:21:30 +0300

    Update sidebar.php
    added styling to sidebar

--- www/sidebar.php
+++ www/sidebar.php
@@ -1,7 +1,35 @@
-<div class="sidebar" style="width: 20%;">
-	<h3 class="bar-item">Menu</h3>
-	<a href="#"	class="sidebar-item button">Link1</a><br>
-	<a href="#"	class="sidebar-item button">Link2</a><br>
-	<a href="#"	class="sidebar-item button">Link3</a><br>
+<div class="sidebar" style="width: 10%;">
+	<a href="#"	class="sidebar-item button">Etusivu</a><br>
+	<a href="#"	class="sidebar-item button">Elokuvat</a><br>
+	<a href="#"	class="sidebar-item button">Kauppa</a><br>
 </div>
 
+<style type="text/css">
+	
+	.sidebar {
+		border-width: 1px;
+		border-style: solid;
+		border-color: black;
+		height: 100%;
+		top:103px;
+		position: fixed;
+ 		z-index: 1;
+ 		left: 0;
+ 		background-color: #03e3fc;
+ 		overflow-x: hidden;
+ 		padding-top: 20px;
+	}
+
+	.sidebar a{
+		padding: 6px 6px 6px 16px;
+		font-size: 25px;
+		display: block;
+		color: #818181;
+		text-decoration: none;
+	}
+
+	.sidebar a:hover {
+		color: black;
+	}
+
+</style>
