7d27b73f9a4b54468dfe143279d3a4a24979a12e
MikHuh <104988925+MikHuh@users.noreply.github.com>
2022-05-13 19:54:45 +0300

    Update header.php

--- www/header.php
+++ www/header.php
@@ -1,14 +1,13 @@
-
-
 <div class="header">
 	<img src="images/FK-Logo.png" alt="Logo">
 	<h1>
-		Funkino
+	   Funkino
 	</h1>
+	<h2>Hae elokuvaa</h2>
 	<h5><form action="site.php" method="post">
 			<input type="text" name="movieName">
 
 			<input type="submit">
-		</form></h5>
-	
+		</form>
+	</h5>	
 </div>
