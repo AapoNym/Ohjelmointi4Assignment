0fad6966b157744080e6476b206d93af93a9b752
MikHuh <104988925+MikHuh@users.noreply.github.com>
2022-05-13 20:36:14 +0300

    Create UudetElokuvatSite.php

---
+++ www/UudetElokuvatSite.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+	<link href="https://fonts.googleapis.com/css2?family=Anybody:wght@200&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="includes/css/style.css">
+	<?php include "header.php" ?>
+	<?php include "sidebar.php" ?>
+</head>
+<body>
+	<div class="UudetElokuvat">
+	<p> UUDET ELOKUVAT</p><br><br>
+	<img src='images/moviePosters/Joker.jpg' alt='Logo'>
+	<p1>Joker</p1>
+	<br>
+	<img src='images/moviePosters/Avengers.jpg' alt='Logo'>
+	<p2>Avengers</p2>
+
+	<?php include "footer.php" ?>
+    
+</div>
+ </body>
+ </html>
