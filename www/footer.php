9b00a29f5213eed0beb61d1a69d3f6b5c35c189c
AapoNym <aapo.nyman@gmail.com>
2022-05-13 18:22:42 +0300

    Moved all css from php files to style.css

--- www/footer.php
+++ www/footer.php
@@ -1,26 +1,3 @@
 <footer class="footer">
 	<h3>Copyright 2022 Funkino</h3>
 </footer>
-
-<style type="text/css">
-
-	.footer{
-		padding: 0 0 0 0;
-		margin: auto;
-		position: relative;
-		border-width: 1px;
-		border-style: solid;
-		border-color: black;
-		bottom: 0;
-		right: 0;
-		width: 100%;
-		background: #25a5be;
-	}
-
-	.footer h3{
-		padding: 0 10px 0 0;
-		text-align: right;
-		font-size: 20px;
-		color: black;
-	}
-	</style>
--- www/sidebar.php
+++ www/sidebar.php
@@ -1,40 +1,11 @@
+<?php $page = substr($_SERVER['SCRIPT_NAME'], strrpos($_SERVER['SCRIPT_NAME'],"/")+1); ?>
+
 <div class="sidebar" style="width: 200px;">
-	<a href="#"	class="sidebar-item button">Etusivu</a><br>
-	<a href="#"	class="sidebar-item button">Uusimmat Elokuvat</a><br>
-	<a href="#"	class="sidebar-item button">Teatterissa Nyt</a><br>
-</div>
-
-<style type="text/css">
 	
-	.sidebar {
-		border-width: 1px;
-		border-style: solid;
-		border-color: black;
-		height: 100%;
-		top:103px;
-		position: fixed;
- 		z-index: 1;
- 		left: 0;
- 		background-color: #29b2cc;
- 		overflow-x: hidden;
- 		padding-top: 20px;
-	}
+	<a href="site.php"	class="sidebar-item button">Etusivu</a><br>
 
-	.sidebar a{
-		border-style: solid;
-		border-color: black;
-		border-width: 1px;
-		background-color: #cfbe88 ;
-		padding: 6px 6px 6px 16px;
-		font-size: 20px;
-		display: block;
-		color: #222324;
-		text-decoration: none;
-	}
+	<a href="#"	class="sidebar-item button">Uusimmat Elokuvat</a><br>
 
-	.sidebar a:hover {
-		color: black;
-		background-color: #e3cc81;
-	}
+	<a href="#"	class="sidebar-item button">Teatterissa Nyt</a><br>
 
-</style>
+</div>
