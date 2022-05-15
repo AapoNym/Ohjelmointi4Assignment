463ec3ceaedeefdbd7eab9efa916064f79a80bf0
MikHuh <104988925+MikHuh@users.noreply.github.com>
2022-05-15 17:15:10 +0300

    Update categorySite.php

--- www/categorySite.php
+++ www/categorySite.php
@@ -1,3 +1,5 @@
+<?php include "article-header.php";?>
+<?php session_start(); ?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,44 +15,112 @@
 	<?php include "sidebar.php" ?>
   
    <main>
-   	 <div style="margin-left: 200px;">
-
+   	 
+		 
+	<div class="category">
+		<form action="categorySite.php" method="post">
+			<div>
+	    		<label for = "category"></label>
+	    		<select name = "category" id = "category">
+	    			<option value =""> --- Valitse kategoria --- </option>
+	    			<option value="Komedia">Komedia</option>
+	    			<option value="Toiminta">Toiminta</option>
+	    			<option value="Kauhu">Kauhu</option>
+	    			<option value="Lapsille">Lapsille</option>
+	    			<option value="Draama">Draama</option>
+	    		</select>
+	  		</div>
+	   		<div>
+	    		<br><button type="submit">Lähetä</button>
+	    	</div>
+	 	</form>
+	</div>
+	   <div style="margin-left: 200px;">
    	 	<div class="categorySite">
 	<?php
 	$category = $_POST['category'];
+
+  //This will be shown if "Komedia" is picked as category
 	if ($category == "Komedia"){
-	echo "<p1>Kategoria: Komedia</p1>" . "<br><br>".
-	"<img src='images/moviePosters/BridgetJones.jpg' alt='Logo'>";
-	echo "<p2>BridgetJones</p2>";
-	echo "<br>" .
-	"<img src='images/moviePosters/Joker.jpg' alt='Logo'>";
-	echo "<p3>Joker</p3>";
-     }
+
+	echo "<p1>Kategoria: Komedia</p1>" . "<br><br>";
+ 
+  $movieBridgetJones = $_SESSION["movieBridgetJones"];
+
+  echo "<div class='moviePreview'>";
+  echo "<img src='images/moviePosters/BridgetJones.jpg'>";
+  $movieBridgetJones->displayMovie();
+  echo "<button>Liput -></button>";
+	echo "</div>";
+	echo "<br>";
+  }
 	?>
+
     <?php
+    // This is Shown if "kauhu" is picked as category
 	if ($category == "Kauhu"){
-		echo "<p1>Kategoria: Kauhu</p1>". "<br><br>" .
-	"<img src='images/moviePosters/NightmareOnElmStreet.jpg' alt='Logo'>" .
-	"<p2>Nightmare On Elm Street</p2>";
-	echo "<br>" .
-	"<img src='images/moviePosters/ScaryMovie.jpg' alt='Logo'>"
-	. "<p3>Scary Movie</p3>";
+	echo "<p1>Kategoria: Kauhu</p1>". "<br><br>";
+	$movieNightmareOnElmStreet = $_SESSION['movieNightmareOnElmStreet'];
+
+  echo "<div class='moviePreview'>";
+	echo "<img src='images/moviePosters/NightmareOnElmStreet.jpg' alt='Logo'>";
+	
+	$movieNightmareOnElmStreet->displayMovie();
+	echo "<button>Liput -></button>";
+	echo "</div>";
+
+	echo "<br>";
+	echo "<div class='moviePreview'>";
+	echo "<img src='images/moviePosters/ScaryMovie.jpg' alt='Logo'>";
+	$movieScaryMovie = $_SESSION['movieScaryMovie'];
+	$movieScaryMovie->displayMovie();
+	echo "<button>Liput -></button>";
+	echo "</div>";
      }
 	?>
 
     <?php
+   // This is Shown if "Lapsille" is picked as category
 	if ($category == "Lapsille"){
-		echo "<p1>Kategoria: Lapsille</p1>" . "<br><br>".
-	"<img src='images/moviePosters/BeeMovie.jpg' alt='Logo'>";
-	echo "<p2>Bee Movie</p2>";
+	echo "<p1>Kategoria: Lapsille</p1>" . "<br><br>";
+    $movieBeeMovie = $_SESSION['movieBeeMovie'];
+
+    echo "<div class='moviePreview'>";
+	echo "<img src='images/moviePosters/BeeMovie.jpg' alt='Logo'>";
+	$movieBeeMovie->displayMovie();
+	echo "<button>Liput -></button>";
+	echo "</div>";
      }
 	?>
-    <?php
+   
+	 <?php
+    // This is Shown if "Toiminta" is picked as category
 	if ($category == "Toiminta"){
-		echo "<p1>Kategoria: Toiminta</p1>";
-	    echo "<br><br>";
-	    echo "<img src='images/moviePosters/Avengers.jpg' alt='Logo'>";
-	    echo "<p2>Avengers</p2>";
+	echo "<p1>Kategoria: Toiminta</p1>";
+	echo "<br><br>";
+	echo "<div class='moviePreview'>";
+	echo "<img src='images/moviePosters/Avengers.jpg' alt='Logo'>";
+	$movieAvengers = $_SESSION["movieAvengers"];
+	$movieAvengers->displayMovie();
+	echo "<button>Liput -></button>";
+	echo "</div>";
+	echo "<br>";
+	}
+	?>
+
+	 <?php
+    // This is Shown if "Draama" is picked as category
+	if ($category == "Draama"){
+	echo "<p1>Kategoria: Draama</p1>";
+	echo "<br><br>";
+	$movieJoker = $_SESSION["movieJoker"];
+	echo "<div class='moviePreview'>";
+	echo "<img src='images/moviePosters/Joker.jpg' alt='Logo'>";
+	
+	$movieJoker->displayMovie();
+	echo "<button>Liput -></button>";
+	echo "</div>";
+	echo "<br>";
 	}
 	?>
     <?php
